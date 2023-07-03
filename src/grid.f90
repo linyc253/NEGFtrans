40edee4c9a10c9e6f562f2e04a64e8208271ee06
linyc253 <linyc253@gmail.com>
2023-07-04 00:27:22 +0800

    Add subroutine LocalPotential_RealToPlanewave

--- src/grid.f90
+++ src/grid.f90
@@ -7,22 +7,25 @@ module grid
 contains
     subroutine LocalPotential_RealToPlanewave(V_real, V_reciprocal)
         real*8, intent(in) :: V_real(:, :)
-        complex*16, intent(out) :: V_reciprocal(:, :)
+        complex*16, allocatable, intent(inout) :: V_reciprocal(:, :)
+        !                ^                 ^
+        ! Becuase we want to pass the boundary of V_reciprocal to this subroutine
 
         real*8, allocatable :: work(:, :)
         complex*16, allocatable :: work_out(:, :)
-        integer :: N_x, N_y
+        integer :: N_x, N_y, i, j, N_prod
         type(C_PTR) :: plan
 
         N_x = size(V_real, 1)
         N_y = size(V_real, 2)
+        N_prod = N_x * N_y
         allocate(work(N_x, N_y))
         allocate(work_out(N_x / 2 + 1, N_y))
 
         ! Times phase factor
         do i=1, N_x
             do j=1, N_y         
-                work(i, j) = V_real(i, j) * (-1) ** (j)
+                work(i, j) = V_real(i, j) * (-1) ** (j - 1)
             end do
         end do
 
@@ -32,9 +35,15 @@ contains
         call fftw_execute_dft_r2c(plan, work, work_out)
 
         ! Post Process
-        do i=1, N_x / 2
-            do j=1, N_y       
-                V_reciprocal(i, j) = work_out(N_x / 2 - i, j) / (N_x * N_y)
+        do i=0, N_x / 2
+            do j=-N_y / 2, N_y / 2
+                V_reciprocal(i, j) = work_out(i + 1, j + 1 + N_y / 2) / N_prod
+            end do
+        end do
+
+        do i=-N_x / 2, -1
+            do j=-N_y / 2, N_y / 2
+                V_reciprocal(i, j) = conjg(V_reciprocal(-i, -j))
             end do
         end do
     end subroutine LocalPotential_RealToPlanewave
